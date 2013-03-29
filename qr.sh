496f37fe5deeb58144e57a767ddf619954b38e4c
Aaron Wolen <aaron@wolen.com>
2013-03-29 16:12:19 -0300

    Initial commit of qR

---
+++ qr.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#export r script
+scriptname=$1
+
+#remove extensions .R
+scriptname="${scriptname%%.[R|r]}"
+SCRIP=${1:0:12}
+
+echo "R script is" $scriptname
+sed s/Rpbs/$SCRIP/g < /usr/local/bin/qRthw > qRscripta-$scriptname
+
+sed s/input/$scriptname/g < qRscripta-$scriptname > qRscript-$scriptname
+
+/opt/pbs/default/bin/qsub -k n -c c=1 -j oe qRscript-$scriptname
+rm qRscripta-$scriptname
+rm qRscript-$scriptname
+echo "QR is batching a job for you"; echo "See output in $scriptname.Rout"
+
+sleep 2
+rm -f R-$scriptname.o*
