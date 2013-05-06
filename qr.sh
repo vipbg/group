bd2e05e1e25d9a3b2777290f5f88834db02451d0
Aaron Wolen <aaron@wolen.com>
2013-05-06 09:56:12 -0400

    Add comments

--- qr.sh
+++ qr.sh
@@ -1,19 +1,25 @@
-#!/bin/bash
-#export r script
+# !/bin/bash
+
+# export r script
 scriptname=$1
 
-#remove extensions .R
+# remove extensions .R
 scriptname="${scriptname%%.[R|r]}"
 SCRIP=${1:0:12}
 
 echo "R script is" $scriptname
-sed s/Rpbs/$SCRIP/g < /usr/local/bin/qRthw > qRscripta-$scriptname
 
+# Construct submission script
+sed s/Rpbs/$SCRIP/g < /usr/local/bin/qRthw > qRscripta-$scriptname
 sed s/input/$scriptname/g < qRscripta-$scriptname > qRscript-$scriptname
 
+# Submit
 /opt/pbs/default/bin/qsub -k n -c c=1 -j oe qRscript-$scriptname
+
+# Clean-up
 rm qRscripta-$scriptname
 rm qRscript-$scriptname
+
 echo "QR is batching a job for you"; echo "See output in $scriptname.Rout"
 
 sleep 2
